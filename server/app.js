1a5cc0523d8cf8ab4fc6a354b71b80ec51a0b3a7
Sahil Gawade <104153594+Sahil-2005@users.noreply.github.com>
2025-12-14 13:40:07 +0530

    updated packages in frontend side

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
-require("dotenv").config();
 
 const resumeRoutes = require("./routes/resumeRoutes");
 const authRoutes = require("./routes/authRoutes");
